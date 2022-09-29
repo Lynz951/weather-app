471deae32f7a9fd3b83172f471ec9e228de67595
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-09-29 16:04:11 +0000

    button and input

--- main.js
+++ main.js
@@ -1,66 +1,56 @@
-// window.onload = function() {
-//     document.getElementById("sendButton").onclick = function() {
-//         //get zip from text field
-//         const zip = document.getElementById("zipCodeInput").value;
-//         //call 
-//         createElements();
-//     }
-// }
 
 document.body.onload = createElements;
 
-function createElements() {
+var button = document.createElement('input');
+button.type = 'button';
+button.id = 'submit';
+button.value = 'Submit';
+button.className = 'btn';
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?zip=40508,us&appid=369d23ad5471bf036f577bbec69ebdd3`;
-    // const tempElement = document.getElementById("Temperature");
-    // const cityElement = document.getElementById("City");
-    // const condElement = document.getElementById("Conditions");
+    const zip = document.getElementById("zipCodeInput").value;
+
+  
+ 
+    button.onclick = function() {
+        const zip = document.getElementById("zipCodeInput").value;
+        console.log("zipcode",zip);
+        createElements(zip);
+    };
+ 
+    var container = document.getElementById('root');
+    container.appendChild(button);
+
+
+function createElements(zip) {
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=369d23ad5471bf036f577bbec69ebdd3`;
     
 
     axios.get(url)
 
         .then(function (response) {
+
             const root = document.getElementById('root');
-            const cityElement = document.createTextNode(`City: ${response.data.name}`);
-
-            root.appendChild(cityElement);
-
             const currentDiv = document.getElementById("root");
-                document.body.insertBefore(root, currentDiv);
+            document.body.insertBefore(root, currentDiv);
 
+            const cityElement = document.createTextNode(`City: ${response.data.name}`);
             const tempElement = document.createTextNode(`Temperature: ${response.data.main.temp}°Kelvin`);
-            // tempElement.style.color = "black";
-
-            root.appendChild(tempElement);
-
             const condElement = document.createTextNode(`Conditions: ${response.data.weather[0].description}`);
-
-            root.appendChild(condElement);
-
-
             const weatherIcon = response.data.weather[0].icon;
             const pageIcon = document.createElement('img');
 
-
             let iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
             pageIcon.src = iconUrl;
+
+            root.appendChild(cityElement);
+            root.appendChild(tempElement);
+            root.appendChild(condElement);
             root.appendChild(pageIcon);
 
         })
 
-            // const weatherIcon = document.createElement(`Img: ${response.data.weather[0].icon}`);
-
-            //  let iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-
-            // weatherIcon.src = iconUrl;
-
-        //     root.appendChild(pageIcon);  
-
-        // })
-
         .catch(function (error) {
-            // tempElement.innerText = error;
-            // tempElement.style.color = "red";
             console.log(error);
         })
 
@@ -70,11 +60,3 @@ function createElements() {
     }
     
 
-// window.onload = function() {
-//     document.getElementById("sendButton").onclick = function() {
-//         //get zip from text field
-//         const zip = document.getElementById("zipCodeInput").value;
-//         //call 
-//         createElements();
-//     }
-// }
